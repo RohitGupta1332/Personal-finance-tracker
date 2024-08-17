78af12345b22a9dc36109307c19c01e797ee4e9f
Sajal Chitlangia <schitlangia26@gmail.com>
2024-08-17 12:46:34 +0530

    Fixed

--- Code/frontend/loadSidebar.js
+++ Code/frontend/loadSidebar.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    fetch("index.html")
+    fetch("Sidebar/index.html")
     .then(response => response.text())
     .then(data => {
         document.body.insertAdjacentHTML('afterbegin', data)
