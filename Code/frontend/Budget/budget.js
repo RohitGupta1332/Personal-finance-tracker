d027629f6b81edc08fdede7ae5ba31cf39860958
AnjaliSharma-1 <mail.anjalisharma108@gmail.com>
2024-09-23 21:36:53 +0530

    Changed js for budget page

--- Code/frontend/Budget/budget.js
+++ Code/frontend/Budget/budget.js
@@ -1,21 +1,46 @@
+const userData = JSON.parse(localStorage.getItem('userData'));
 
-//dropdown
-const dropDowns = document.querySelectorAll('.chevron-icon');
-const dropDownMenus = document.querySelectorAll('.dropdown');
-dropDowns.forEach((dropDown, index) => {
-    dropDown.addEventListener('click', () => {
-        // Toggle the form visibility
-        dropDownMenus[index].style.display = dropDownMenus[index].style.display === "grid" ? "none" : "grid";
+document.addEventListener('DOMContentLoaded', () => {
+
+    const addButtons = document.querySelectorAll('.add');
+    const categoryForms = document.querySelectorAll('.category-form');
+    const categoryRows = document.querySelectorAll('.category-row');
+    const saveButtons = document.querySelectorAll('.save-btn');
+    const cancelButtons = document.querySelectorAll('.cancel-btn');
+
+    // Add new budget event - toggles form visibility
+    addButtons.forEach((addButton, index) => {
+        addButton.addEventListener('click', () => {
+            // Toggle the form visibility
+            categoryForms[index].style.display = categoryForms[index].style.display === "grid" ? "none" : "grid";
+        });
     });
-});
-//calender
-const dateInput = document.getElementById('dateInput');
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const monthIndex = currentDate.getMonth();
-const monthNames = ["January", "February", "March", "April", "May", "June", 
-                    "July", "August", "September", "October", "November", "December"];
-const month = monthNames[monthIndex];
-dateInput.value = `${year}-${String(monthIndex + 1).padStart(2, '0')}`;
-document.querySelector('.date').textContent = `${month} ${year}`;
 
+    
+
+    //dropdown
+    const dropDowns = document.querySelectorAll('.chevron-icon');
+    const dropDownMenus = document.querySelectorAll('.dropdown');
+    dropDowns.forEach((dropDown, index) => {
+        dropDown.addEventListener('click', () => {
+            // Toggle the form visibility
+            dropDownMenus[index].style.display = dropDownMenus[index].style.display === "grid" ? "none" : "grid";
+        });
+    });
+    //calender
+    const dateInput = document.getElementById('dateInput');
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const monthIndex = currentDate.getMonth();
+    const monthNames = ["January", "February", "March", "April", "May", "June", 
+                        "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[monthIndex];
+    dateInput.value = `${year}-${String(monthIndex + 1).padStart(2, '0')}`;
+    document.querySelector('.date').textContent = `${month} ${year}`;
+
+
+
+
+
+
+});
