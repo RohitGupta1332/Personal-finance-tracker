fd8834ca01acb67bdad62cde9bb3caccaf394743
Rohit <rohitgupta7471@gmail.com>
2024-10-24 15:12:50 +0530

    Added testig documentation

--- Code/backend/config/Database.php
+++ Code/backend/config/Database.php
@@ -11,7 +11,7 @@ class Database{
         $this->host = "localhost";
         $this->dbname = "finance_tracker";
         $this->username = "root";
-        $this->password = "";
+        $this->password = "7439rohit";
 
         try{
             $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}",$this->username,$this->password);
